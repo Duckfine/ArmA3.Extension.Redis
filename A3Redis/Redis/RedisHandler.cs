78b25119590fe55a1789dcfaf95e1cf4dc52c154
Daniel Gran <contact@grandaniel.com>
2022-04-12 23:01:29 +0200

    Refactoring

--- A3Redis/Redis/RedisHandler.cs
+++ A3Redis/Redis/RedisHandler.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace A3Redis.Redis
 {
@@ -13,17 +7,15 @@ namespace A3Redis.Redis
   {
     private const int STANDARD_PORT = 6379;
     private const string STANDARD_PASSWORD = "";
-    
-    
+
     private NetworkStream NetworkStream;
     TcpClient TcpClient;
-
 
     private readonly String Hostname;
     private readonly int Port;
     string Password;
 
-    
+
     public RedisHandler(string hostname, int port = STANDARD_PORT, string password = STANDARD_PASSWORD)
     {
       Hostname = hostname;
@@ -31,29 +23,18 @@ namespace A3Redis.Redis
       Password = password;
     }
 
-    #region Connecting
 
+    #region Connecting
     public void Connect()
     {
       TcpClient = new TcpClient();
       TcpClient.Connect(Hostname, Port);
-
-      if (TcpClient.Connected)
-      {
-        Console.WriteLine("Socket nun zum Server verbunden!");
-        NetworkStream = TcpClient.GetStream();
-      }
-      else
-      {
-        Console.WriteLine("Verbindung fehlgeschlagen!");
-      }
+      NetworkStream = TcpClient.GetStream();
     }
 
     public void Disconnect()
     {
       TcpClient.Close();
-
-      Console.WriteLine("Verbindung wurde getrennt");
     }
 
     public void Reconnect()
@@ -62,12 +43,11 @@ namespace A3Redis.Redis
       Connect();
     }
 
-
     public bool CheckConnection()
     {
       try
       {
-        string[] toSend = {"INFO"};
+        string[] toSend = { "INFO" };
         SendBuildCommand(toSend);
         //todo handle response
         return true;
@@ -77,11 +57,10 @@ namespace A3Redis.Redis
         return false;
       }
     }
-
     #endregion Connecting
 
-    #region Core
 
+    #region Core
     public void SendBuildCommand(String[] args)
     {
       StringBuilder output = new StringBuilder("*");
@@ -97,52 +76,37 @@ namespace A3Redis.Redis
       NetworkStream.Write(bytes, 0, bytes.Length);
     }
 
-
     private string GetResponse()
     {
       byte[] bytes = new byte[TcpClient.ReceiveBufferSize];
-
-      // Read can return anything from 0 to numBytesToRead. 
-      // This method blocks until at least one byte is read.
-      NetworkStream.Read(bytes, 0, (int) TcpClient.ReceiveBufferSize);
-
-      // Returns the data received from the host to the console.
+      NetworkStream.Read(bytes, 0, (int)TcpClient.ReceiveBufferSize);
       string returndata = Encoding.UTF8.GetString(bytes);
-
       return returndata;
     }
 
-
     public string HandleResponse()
     {
-      //For Simple Strings the first byte of the reply is "+"
-      //For Errors the first byte of the reply is "-"
-      //For Integers the first byte of the reply is ":"
-      //For Bulk Strings the first byte of the reply is "$"
-      //For Arrays the first byte of the reply is "*"
       String rawResp = GetResponse().Trim('\0');
       rawResp = rawResp.Replace("\n", "");
       String[] proc = rawResp.Split('\r');
 
-
-      if (proc[0].StartsWith("+")) // Simple String
-      {
-        Console.WriteLine("Simple String received");
-        proc[0] = proc[0].Substring(1);
-        return proc[0];
-      } else if (proc[0].StartsWith("-")) // Error
-      {
-        
-      } else if (proc[0].StartsWith(":")) // Integer
+      if (proc[0].StartsWith("+")) // Simple String received
       {
         proc[0] = proc[0].Substring(1);
         return proc[0];
-      } else if (proc[0].StartsWith("$")) // Bulk String (More than 1 string)
+      }
+      else if (proc[0].StartsWith("-")) { } // Error received
+      else if (proc[0].StartsWith(":")) // Integer received
       {
-        if (proc[0] == "$-1\r") return null; //Null returned
+        proc[0] = proc[0].Substring(1);
+        return proc[0];
+      }
+      else if (proc[0].StartsWith("$")) // Bulk String (More than 1 string) received
+      {
+        if (proc[0] == "$-1\r") return null; // Null received 
         return proc[1];
       }
-      else if (proc[0].StartsWith("*")) // array
+      else if (proc[0].StartsWith("*")) // Array received
       {
         int arrSize;
         if (!Int32.TryParse(proc[0].Substring(1), out arrSize)) return rawResp; // Failed
@@ -152,8 +116,6 @@ namespace A3Redis.Redis
         if (arrSize <= 0) return "[]";
 
         string result = "["; // like [1,2,4] in an compilable a3 array
-
-
         for (int i = 1; i < arrSize + 1; i++)
         {
           int sub;
@@ -170,150 +132,119 @@ namespace A3Redis.Redis
 
           tmp = arr[i].Substring(sub);
 
-
-          if (tmp.ToLower() == "false") // bool?
+          if (tmp.ToLower() == "false")
           {
             toAdd = "false";
           }
-          else if (tmp.ToLower() == "true") // bool?
+          else if (tmp.ToLower() == "true")
           {
             toAdd = "true";
           }
-          else if (tmp.ToLower().StartsWith("{") && tmp.ToLower().EndsWith("}")) // check if code is sent
+          else if (tmp.ToLower().StartsWith("{") && tmp.ToLower().EndsWith("}")) // A3 Datatype Code?
           {
             toAdd = tmp;
           }
-          else
+          else // Its a string, so put parentheses around it.
           {
             toAdd = "\"\"" + tmp + "\"\"";
           }
-
-
           result = result + toAdd + ", ";
         }
-
         result = result.Substring(0, result.Length - 2);
         result += "]";
-
-
         return result;
       }
-
       return rawResp;
     }
 
-
     private string SendCommand(int dbid, string[] args)
     {
-      string[] send = {"SELECT", dbid.ToString()};
+      string[] send = { "SELECT", dbid.ToString() };
       SendBuildCommand(send);
       HandleResponse();
       SendBuildCommand(args);
       return HandleResponse();
     }
-
     #endregion Core
 
 
     #region Setters
-
     public string SetString(int dbid, String key, String value)
     {
-      string[] args = {"SET", key, value};
+      string[] args = { "SET", key, value };
       return SendCommand(dbid, args);
     }
 
     public string AddToList(int dbid, String key, String value)
     {
-      string[] args = {"RPUSH", key, value};
+      string[] args = { "RPUSH", key, value };
       return SendCommand(dbid, args);
     }
 
     public string ListUpdate(int dbid, String key, String index, String value)
     {
-      string[] args = {"LSET", key, index, value};
+      string[] args = { "LSET", key, index, value };
       return SendCommand(dbid, args);
     }
 
     public string KeyDelete(int dbid, String key)
     {
-      string[] args = {"DEL", key};
+      string[] args = { "DEL", key };
       return SendCommand(dbid, args);
     }
 
     #endregion Setters
 
+
     #region Getters
 
     public bool KeyExists(int dbid, string key)
     {
-      string[] args = {"EXISTS", key};
-
+      string[] args = { "EXISTS", key };
       string result = SendCommand(dbid, args);
-
-      if (result == "1")
-      {
-        return true;
-      }
-      else
-      {
-        return false;
-      }
+      return result == "1" ? true : false;
     }
 
     public string DBKeys(int dbid, string regex)
     {
-      string[] args = {"KEYS", regex};
+      string[] args = { "KEYS", regex };
       var ret = SendCommand(dbid, args);
       ret = ret.Replace("\"\"", "\"");
       return ret;
     }
 
-
     public string DBFlush(int dbid)
     {
-      string[] args = {"FLUSHDB"};
+      string[] args = { "FLUSHDB" };
       return SendCommand(dbid, args);
     }
-
 
     public string DBSize(int dbid)
     {
-      string[] args = {"DBSIZE"};
-
+      string[] args = { "DBSIZE" };
       string result = SendCommand(dbid, args);
-
       if (int.TryParse(result, out int res) || res < 0)
-      {
         return res.ToString();
-      }
-
       return "-1";
     }
 
-
     public string GetEntry(int dbid, string key)
     {
-      string[] args = {"GET", key};
-
+      string[] args = { "GET", key };
       return SendCommand(dbid, args);
     }
 
-
     public string ListGetEntry(int dbid, string key, string index)
     {
-      string[] args = {"LINDEX", key, index.ToString()};
-
+      string[] args = { "LINDEX", key, index.ToString() };
       return SendCommand(dbid, args);
     }
 
     public string ListGetSize(int dbid, string key)
     {
-      string[] args = {"LLEN", key};
-
+      string[] args = { "LLEN", key };
       return SendCommand(dbid, args);
     }
-
     #endregion
   }
 }
