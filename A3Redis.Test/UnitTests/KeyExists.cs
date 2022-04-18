4d80b1493371cd813e8c3decb7099a3008d14ffe
Daniel Gran <contact@grandaniel.com>
2022-04-18 19:36:31 +0200

    Removed unused imports

--- A3Redis.Test/UnitTests/KeyExists.cs
+++ A3Redis.Test/UnitTests/KeyExists.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using A3Redis.Redis;
+﻿using A3Redis.Redis;
 using Xunit;
 
 namespace A3Redis.Test.UnitTests
