8a553669d6c8baa7df27624f115de4458d92bff5
Daniel Gran <contact@grandaniel.com>
2022-04-18 19:30:51 +0200

    Edited indicies

---
+++ A3Redis.Test/UnitTests/KeyExists.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A3Redis.Redis;
+using Xunit;
+
+namespace A3Redis.Test.UnitTests
+{
+  public class KeyExists
+  {
+    [Fact]
+    public void TestExists()
+    {
+      var str = "exists:0:auth#76561199259412469";
+
+      var re = new RedisExtension(new RedisConnectionTcpClientAdapter());
+      var return_str = re.HandleCommand(str);
+
+
+    }
+
+  }
+}
--- A3Redis/RedisExtension.cs
+++ A3Redis/RedisExtension.cs
@@ -69,68 +69,68 @@ namespace A3Redis
           break;
 #endif
         case "exists":
-          dbid = Int32.Parse(parameter[2]);
-          key = parameter[3];
+          dbid = Int32.Parse(parameter[1]);
+          key = parameter[2];
           bool result = RedisConnection.KeyExists(dbid, key);
           return result ? RETURNTRUE : RETURNFALSE;
 
         case "set":
-          dbid = Int32.Parse(parameter[2]);
-          key = parameter[3];
-          value = parameter[4];
+          dbid = Int32.Parse(parameter[1]);
+          key = parameter[2];
+          value = parameter[3];
           strresult = RedisConnection.SetString(dbid, key, value);
           return strresult == "OK" ? RETURNTRUE : RETURNFALSE;
 
 
         case "get":
-          dbid = Int32.Parse(parameter[2]);
-          key = parameter[3];
+          dbid = Int32.Parse(parameter[1]);
+          key = parameter[2];
           strresult = RedisConnection.GetEntry(dbid, key);
           return strresult;
 
 
         case "dbflush":
-          dbid = Int32.Parse(parameter[2]);
+          dbid = Int32.Parse(parameter[1]);
           return RedisConnection.DBFlush(dbid);
 
 
         case "dbkeys":
-          dbid = Int32.Parse(parameter[2]);
-          key = parameter[3];
+          dbid = Int32.Parse(parameter[1]);
+          key = parameter[2];
           strresult = RedisConnection.DBKeys(dbid, key);
           return strresult;
 
 
         case "dbsize":
-          dbid = Int32.Parse(parameter[2]);
+          dbid = Int32.Parse(parameter[1]);
           strresult = RedisConnection.DBSize(dbid);
           return strresult;
 
 
         case "delete":
-          dbid = Int32.Parse(parameter[2]);
-          key = parameter[3];
+          dbid = Int32.Parse(parameter[1]);
+          key = parameter[2];
           strresult = RedisConnection.KeyDelete(dbid, key);
           return strresult == "1" ? RETURNTRUE : RETURNFALSE;
 
         case "listadd":
-          dbid = Int32.Parse(parameter[2]);
-          key = parameter[3];
-          value = parameter[4];
+          dbid = Int32.Parse(parameter[1]);
+          key = parameter[2];
+          value = parameter[3];
           strresult = RedisConnection.AddToList(dbid, key, value);
           isNumeric = int.TryParse(strresult, out intresult);
           return isNumeric ? RETURNTRUE : RETURNFALSE;
 
         case "listget":
-          dbid = Int32.Parse(parameter[2]);
-          index = parameter[3];
-          key = parameter[4];
+          dbid = Int32.Parse(parameter[1]);
+          index = parameter[2];
+          key = parameter[3];
           strresult = RedisConnection.GetListEntry(dbid, index, key);
           return strresult;
 
         case "listsize":
-          dbid = Int32.Parse(parameter[2]);
-          key = parameter[3];
+          dbid = Int32.Parse(parameter[1]);
+          key = parameter[2];
           strresult = RedisConnection.GetListSize(dbid, key);
           isNumeric = int.TryParse(strresult, out intresult);
           if (isNumeric)
@@ -139,10 +139,10 @@ namespace A3Redis
             return RETURNFALSE;
 
         case "listupdate":
-          dbid = Int32.Parse(parameter[2]);
-          key = parameter[3];
-          index = parameter[4];
-          value = parameter[5];
+          dbid = Int32.Parse(parameter[1]);
+          key = parameter[2];
+          index = parameter[3];
+          value = parameter[4];
           strresult = RedisConnection.ListUpdate(dbid, key, index, value);
 
           return strresult == "OK" ? RETURNTRUE : RETURNFALSE;
